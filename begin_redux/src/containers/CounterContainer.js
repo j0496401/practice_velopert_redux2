c1a40c8be4105d41a724729a8221df8877ff43f5
0pippi0 <j.romany91@gmail.com>
2020-03-04 19:35:21 +0900

    redux 의 bindActionCreator 함수를 사용

--- begin_redux/src/containers/CounterContainer.js
+++ begin_redux/src/containers/CounterContainer.js
@@ -27,18 +27,12 @@ class CounterContainer extends Component {
     }
 }
 
-// props 값으로 넣어 줄 상태를 정의해줍니다.
-const mapStateToProps = (state) => ({
-    number: state.counter.number
-})
-
-// props 값으로 넣어 줄 액션 함수들을 정의해줍니다
-const mapDispatchToProps = (dispatch) => ({
-    increment: () => dispatch(counterActions.increment()),
-    decrement: () => dispatch(counterActions.decrement())
-})
-
-// 컴포넌트를 리덕스와 연동 할 때에는 connect 를 사용합니다.
-// connect() 의 결과는, 컴포넌트에 props 를 넣어주는 함수를 반환합니다.
-// 반환된 함수에 우리가 만든 컴포넌트를 넣어주면 됩니다.
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default connect(
+    (state) => ({
+        number: state.counter.number
+    }),
+    (dispatch) => ({
+        increment: () => dispatch(counterActions.increment()),
+        decrement: () => dispatch(counterActions.decrement())
+    })
+)(CounterContainer);
