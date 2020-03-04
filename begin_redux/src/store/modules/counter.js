675c50492f766e6a0511309319ae3904d858e6eb
0pippi0 <j.romany91@gmail.com>
2020-03-04 17:33:07 +0900

    <redux-actions 의 createAction 과 handleActions 사용하기> redux-actions 의 createAction 이라는 함수를 사용 handleActions 를 사용하면, 리듀서 코드를 조금 더 깔끔하게 작성

--- begin_redux/src/store/modules/counter.js
+++ begin_redux/src/store/modules/counter.js
@@ -1,11 +1,13 @@
+import { createAction } from 'redux-actions';
+
 // 액션 타입을 정의해줍니다.
 const INCREMENT = 'counter/INCREMENT';
 const DECREMENT = 'connter/DECREMENT';
 
 // 액션 생성 함수를 만듭니다.
 // 이 함수들은 나중에 다른 파일에서 불러와야 하므로 내보내줍니다.
-export const increment = () => ({ type: INCREMENT });
-export const decrement = () => ({ type: DECREMENT });
+export const increment = 'counter/INCREMENT';
+export const decrement = 'counter/DECREMENT';
 
 // 모듈의 초기 상태를 정의합니다.
 const initialState = {
